13d98647db04c2b8fffc5cee68674d6812c79df2
Eliav Menachi <eliav.menachi@gmail.com>
2024-11-02 20:41:58 +0200

    add api

--- App.js
+++ App.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const app = express();
 //change port
-const port = 3001;
+const port = 3003;
+
+app.get("/test", (req, res) => {
+  res.send("Hello World!!!");
+});
 
 app.get("/", (req, res) => {
   res.send("Hello World!!!");
